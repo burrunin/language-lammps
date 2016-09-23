2e322a48f40898d67f5900f5acb2b035ab003fe1
bruno@mpbd2 <bruno.duran.rey@gmail.com>
2016-09-23 16:03:24 +0100

    removed (hopefully) redundant code

--- spec/language-lammps-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageLammpsView from '../lib/language-lammps-view';
-
-describe('LanguageLammpsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
